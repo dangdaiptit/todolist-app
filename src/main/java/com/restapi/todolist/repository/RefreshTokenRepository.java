d09e06d3a0b3cdc88e019e1893f24bfcae12ecba
Dai <dangdai.ptit@gmail.com>
2023-06-09 17:55:39 +0700

    make it better

--- src/main/java/com/restapi/todolist/repository/RefreshTokenRepository.java
+++ src/main/java/com/restapi/todolist/repository/RefreshTokenRepository.java
@@ -4,9 +4,11 @@ import com.restapi.todolist.models.RefreshToken;
 import com.restapi.todolist.models.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findRefreshTokenByToken(String token);
 
